6ad22d47d0ead9c873d361f65f65d546cb520e17
Carlos Antonio Reis da Silva <carlosreis10.06@hotmail.com>
2020-03-28 12:20:57 -0300

    updates de bugs

--- Backend/src/app/controllers/IncidentController.js
+++ Backend/src/app/controllers/IncidentController.js
@@ -1,52 +1,59 @@
-import connection from '../../database/connection';
+import connection from "../../database/connection";
 
 class IncidentController {
-  async store(req,res){
-    const {title, description ,value,} = req.body;
+  async store(req, res) {
+    const { title, description, value } = req.body;
     const ong_id = req.headers.authorization;
 
-    const [id] = await connection('incidents').insert({
+    const [id] = await connection("incidents").insert({
       title,
       description,
       value,
-      ong_id,
+      ong_id
     });
     return res.json({ id });
-  } 
-  async index(req,res){
-    const { page = 1 } = req.query;
-    const [count] = await connection('incidents').count();
-        
-    const incidents = await connection('incidents')
-    .join('ongs', 'ongs.id', '=','incidents.ong_id')
-    .limit(5)
-    .offset((page - 1)*5)
-    .select([
-      'incidents.*',
-      'ongs.name',
-      'ongs.email',
-      'ongs.whatsapp',
-      'ongs.city',
-      'ongs.uf'
-    ]);
-
-    res.header('X-Total-Count',count['count(*)']);
-
-    return res.json({incidents})
   }
-  async delete(req,res){
+  async index(req, res) {
+    const { page = 1 } = req.query;
+    const [count] = await connection("incidents").count();
+
+    const incidents = await connection("incidents")
+      .join("ongs", "ongs.id", "=", "incidents.ong_id")
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        "incidents.*",
+        "ongs.name",
+        "ongs.email",
+        "ongs.whatsapp",
+        "ongs.city",
+        "ongs.uf"
+      ]);
+
+    res.header("X-Total-Count", count["count(*)"]);
+
+    return res.json({ incidents });
+  }
+  async delete(req, res) {
     const { incident_id } = req.params;
     const ong_id = req.headers.authorization;
 
-    const incidents = await connection('incidents')
-    .where('id',incident_id)
-    .select('ong_id').first();
+    const incidents = await connection("incidents")
+      .where("id", incident_id)
+      .select("ong_id")
+      .first();
 
-    if(incidents.ong_id !== ong_id || !incidents){
-      return res.status(401).json({error:'Operation not permitted'})
+    if(!incidents){
+      return res.status(401).json({ error: "incidents not exist in database" });
+    }  
+
+    if (incidents.ong_id !== ong_id) {
+      return res.status(401).json({ error: "Operation not permitted" });
     }
 
-    await connection('incidents').where('id', incident_id).delete();
+    await connection("incidents")
+      .where("id", incident_id)
+      .delete();
 
     return res.status(204).send();
   }
